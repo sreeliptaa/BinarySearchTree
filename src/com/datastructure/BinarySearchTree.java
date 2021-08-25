e6b2d95e211b5bda349878c76a9b9acf3c0fa624
sreeliptaa <sreelipta65@gmail.com>
2021-08-25 23:44:21 +0530

    Added: wecome message

---
+++ src/com/datastructure/BinarySearchTree.java
@@ -0,0 +1,7 @@
+package com.datastructure;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Binary Search Tree Program");
+    }
+}
